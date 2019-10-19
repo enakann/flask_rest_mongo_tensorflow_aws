bf35f41e56e5c954a2dcfb8bc52a843682a21b12
Navaneeth kannan <Navaneethan55@gmail.com>
2019-10-19 22:54:58 +0530

    Created Sentence API

--- web/app.py
+++ web/app.py
@@ -7,13 +7,82 @@ api = Api(app)
 
 client=MongoClient("mongodb://db:27017")
 
-db=client.navidb
+#db=client.navidb
 
-UserNum=db["UserNum"]
+#UserNum=db["UserNum"]
 
-UserNum.insert({
-    'num_of_users':0
-    })
+#UserNum.insert({
+#    'num_of_users':0
+#    })
+
+
+
+db=client.SentencesDatabase
+users=db["Users"]
+
+
+class Register(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+        hashed_pw = bcrypt.hashpw(password,bcryt.gensalt())
+
+        users.insert({
+            "Username":username,
+            "Password":hashed_pw,
+            "Sentence":"",
+            "Tokens":6
+        })
+
+        retJson = {
+            "status":200,
+            "msg":"Successfully signed up for the API"
+        }
+
+        return jsonify(retJson)
+
+class Sentence(Resource):
+    def post(self):
+        #step1: get the posted data
+        postedData=request.get_json()
+        #step2: read the data
+
+        username = postedData["username"]
+        password = postedData["password"]
+        sentence = postedData["sentence"]
+
+        #step3 verify the username pw match
+        correct_pw = verifyPw(username,password)
+
+        if not correct_pw:
+            retJson={
+                "status":302
+            }
+            return jsonify(retJson)
+
+        #step4 verify user has enough tokens
+
+        num_tokens = countTokens(username)
+        if num_tokens <= 0:
+            return jsonify({"status":301})
+        #step5 store the sentence and return 200
+
+        users.update({
+            "Username":username
+        },{
+            "$set":{"Sentence":sentence,
+                    "Tokens": num_tokens -1}
+        })
+
+        return jsonify({
+            "status":200,
+            "msg":"sentence saved successfully"
+        })
+
+
 
 
 class Visit(Resource):
@@ -173,6 +242,7 @@ api.add_resource(Subtract, "/subtract")
 api.add_resource(Multiply, "/multiply")
 api.add_resource(Divide, "/division")
 api.add_resource(Visit,"/hello")
+api.add_resource(Register,"/register")
 
 @app.route('/')
 def hello_world():
